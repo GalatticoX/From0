01ea9e10f6dd3eea21cc6132bae12b55de94acd7
GalatticoX <162441756+GalatticoX@users.noreply.github.com>
2026-05-03 10:20:15 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let bottom = document.querySelector(".bottom");
-let header = document.querySelector("h1");
-let hero = document.querySelector(".hero");
-
-bottom.addEventListener("click", function() {
-    alert("Hai cliccato il pulsante!");
-    header.textContent = "Sistema Attivato!";
-    hero.classList.toggle("attivo");
-});
